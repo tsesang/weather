84fa53516ff3e7f2361bdd43e1983839a105002e
tsesang <81279457+tsesang@users.noreply.github.com>
2023-06-21 03:06:00 +0530

    added null condition incase city not selected

--- index.js
+++ index.js
@@ -11,43 +11,50 @@ function showWeather() {
     })
     .then((value) => {
         weatherInfo=value;
-        document.getElementById('table').innerHTML=`
-        <table border="1">
-        <tr class="heading">
-            <th colspan="4"><h2>weather of ${cityList.value}</h2></th>
-        </tr>
-        <tr>
-            <th>temperature</th>
-            <th>wind</th>
-            <th>description</th>
-        </tr>
-        <tr>
-            <td>${weatherInfo.temperature}</td>
-            <td>${weatherInfo.wind}</td>
-            <td>${weatherInfo.description}</td>
-    
-        </tr>
-        <tr class="heading">
-            <th colspan="4"><h2>forecast</h2></th>
-        </tr>
-        <tr>
-            <th>day 1</th>
-            <td>temperature ${weatherInfo.forecast[0].temperature}</td>
-            <td>wind ${weatherInfo.forecast[0].wind}</td>
-        </tr>
-        <tr>
-            <th>day 2</th>
-            <td>temperature ${weatherInfo.forecast[1].temperature}</td>
-            <td>wind ${weatherInfo.forecast[1].wind}</td>
-        </tr>
-        <tr>
-            <th>day 3</th>
-            <td>temperature ${weatherInfo.forecast[2].temperature}</td>
-            <td>wind ${weatherInfo.forecast[2].wind}</td>
-        </tr>
-    
-    </table>
-        `
+        console.log(weatherInfo)
+        if(weatherInfo.temperature=== ""){
+            document.getElementById('table').innerHTML=`<p>404 city not found ...</p>`;
+        }
+        else{
+            document.getElementById('table').innerHTML=`
+            <table border="1">
+            <tr class="heading">
+                <th colspan="4"><h2>weather of ${cityList.value}</h2></th>
+            </tr>
+            <tr>
+                <th>temperature</th>
+                <th>wind</th>
+                <th>description</th>
+            </tr>
+            <tr>
+                <td>${weatherInfo.temperature}</td>
+                <td>${weatherInfo.wind}</td>
+                <td>${weatherInfo.description}</td>
+        
+            </tr>
+            <tr class="heading">
+                <th colspan="4"><h2>forecast</h2></th>
+            </tr>
+            <tr>
+                <th>day 1</th>
+                <td>temperature ${weatherInfo.forecast[0].temperature}</td>
+                <td>wind ${weatherInfo.forecast[0].wind}</td>
+            </tr>
+            <tr>
+                <th>day 2</th>
+                <td>temperature ${weatherInfo.forecast[1].temperature}</td>
+                <td>wind ${weatherInfo.forecast[1].wind}</td>
+            </tr>
+            <tr>
+                <th>day 3</th>
+                <td>temperature ${weatherInfo.forecast[2].temperature}</td>
+                <td>wind ${weatherInfo.forecast[2].wind}</td>
+            </tr>
+        
+        </table>
+            `
+        }
+
     });
 
 }
